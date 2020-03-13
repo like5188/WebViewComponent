368aa77454a656e7a3cbed849eeabf60e00b95b3
like <like@adtech.com.cn>
2020-03-13 15:46:03 +0800

    完善

--- component/src/main/java/com/like/webview/component/WebViewApplication.kt
+++ component/src/main/java/com/like/webview/component/WebViewApplication.kt
@@ -2,17 +2,17 @@ package com.like.webview.component
 
 import android.app.Application
 import android.content.Context
-import android.util.Log
 import com.like.common.base.IModuleApplication
+import com.like.common.util.Logger
 import com.tencent.smtt.sdk.QbSdk
 
-class MyApplication : IModuleApplication {
+class WebViewApplication : IModuleApplication {
     override fun attachBaseContext(base: Context?) {
     }
 
     override fun onCreate(application: Application) {
         QbSdk.initX5Environment(application, null)
-        Log.d("MyApplication", "webview component onCreate")
+        Logger.d("WebViewApplication onCreate")
     }
 
     override fun onTerminate(application: Application) {
