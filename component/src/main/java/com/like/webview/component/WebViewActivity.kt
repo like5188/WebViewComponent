f6e9eb6ded4dcc79926bf6eccaa6a7625297e2f1
like <like@adtech.com.cn>
2020-10-19 09:18:01 +0800

    升级库

--- component/src/main/java/com/like/webview/component/WebViewActivity.kt
+++ component/src/main/java/com/like/webview/component/WebViewActivity.kt
@@ -2,11 +2,10 @@ package com.like.webview.component
 
 import android.graphics.PixelFormat
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.like.common.base.BaseAutoWiredActivity
 import com.like.common.base.addFragments
 import com.like.common.util.AutoWired
-import com.like.common.util.injectForIntentExtras
 import com.like.common.util.startActivityByApplication
 import com.like.webview.component.databinding.WebviewActivityWebviewBinding
 
@@ -14,7 +13,7 @@ import com.like.webview.component.databinding.WebviewActivityWebviewBinding
  * 对 [WebViewFragment] 的封装
  * 用于简单显示网页，而不进行交互，如果需要交互，请使用 [WebViewFragment]
  */
-class WebViewActivity : AppCompatActivity() {
+class WebViewActivity : BaseAutoWiredActivity() {
     @AutoWired
     var url: String? = null
 
@@ -35,7 +34,6 @@ class WebViewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         // 网页中的视频，上屏幕的时候，可能出现闪烁的情况，需要如下设置：Activity在onCreate时需要设置:
         window.setFormat(PixelFormat.TRANSLUCENT)
-        injectForIntentExtras()
         mBinding
         url?.let {
             addFragments(R.id.fragment_holder, 0, WebViewFragment(it))
