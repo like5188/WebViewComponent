462e124b4ad63ea320c539179035cea6c7d95d49
like <260864866@qq.com>
2021-03-12 12:02:40 +0800

    升级webview库

--- component/src/main/java/com/like/webview/component/WebViewFragment.kt
+++ component/src/main/java/com/like/webview/component/WebViewFragment.kt
@@ -19,21 +19,6 @@ import com.tencent.smtt.sdk.WebView
  * 1、WebViewFragment 中的方法必须在 initViews() 方法执行完毕之后调用才有效。
  * 因为 FragmentTransaction 的 commit() 方法是异步的，所以我们不知道什么时候 WebViewFragment 会被创建并添加到 Activity 中。
  * 所以，如果 Activity 在 onCreate() 方法中添加了 WebViewFragment，那么就需要在 onStart()或者onResume()方法中调用相关方法才有效，具体情况有所不同。
- *
- * 2、如果需要修改 WebView 相关的样式：
- *      ①修改错误页面
- *          在 layout 文件夹中自定义错误页面布局：
- *          webview_error_view.xml
- *      ②修改进度条颜色
- *          在 colors.xml 文件中定义：
- *          <color name="webview_progress_bar_bg_color">#E6E6E6</color>
- *          <color name="webview_progress_bar_progress_color">#FF4081</color>
- *      ③修改进度条高度
- *          在 values 文件夹中新增 dimen.xml 文件，然后在其中定义：
- *          <?xml version="1.0" encoding="utf-8"?>
- *          <resources>
- *          <dimen name="webview_progress_bar_height">3dp</dimen>
- *          </resources>
  */
 class WebViewFragment(private val url: String?) : BaseLazyFragment() {
     private var mX5ProgressBarWebView: X5ProgressBarWebView? = null
